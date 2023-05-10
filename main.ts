e6dc04d9ab8bafeabda4282c91954ea09e244b17
antonycyr <132452200+antonycyr@users.noreply.github.com>
2023-05-10 11:10:00 +0200

    Archivos iniciales para el proyecto MakeCode

---
+++ main.ts
@@ -0,0 +1,8 @@
+basic.showString("Hola Grupo")
+basic.pause(500)
+for (let index = 0; index < 4; index++) {
+    basic.showIcon(IconNames.Heart)
+    basic.showIcon(IconNames.SmallHeart)
+}
+basic.pause(500)
+basic.showString("Adios Grupo")
